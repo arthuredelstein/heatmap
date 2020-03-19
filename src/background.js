a3222961fd8758c01a5088df31267ee610de59b4
Arthur Edelstein <arthuredelstein@gmail.com>
2020-03-19 10:52:42 -0700

    first commit

---
+++ src/background.js
@@ -0,0 +1,12 @@
+const winEnum = Services.wm.getEnumerator(null);
+const aWindow = winEnum.getNext();
+// Usually figures out the element involved in a click.
+aWindow.addEventListener("click", e => console.log("click from chrome window", e));
+aWindow.addEventListener("command", e => console.log("command received", e));
+
+// registering telemetry events
+await browser.telemetry.registerEvents("test_category", {"test_category": {"methods":["click"], "objects":["click_object"], "extra_keys":["selector"]}})
+
+// recording events
+await browser.telemetry.recordEvent("test_category", "click", "click_object", "dynamicValue", {"selector": "dynamicSelector"})
+
