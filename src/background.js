d5ba01e10ac0c19d40f5748489dda1725ef7815c
Arthur Edelstein <arthuredelstein@gmail.com>
2020-03-23 00:03:50 -0700

    monitor clicks

--- src/background.js
+++ src/background.js
@@ -2,12 +2,16 @@ console.log("heatmap started");
 
 (async () => {
   // registering telemetry events
-  await browser.telemetry.registerEvents("test_category", {"test_category": {"methods":["click"], "objects":["click_object"], "extra_keys":["selector"]}})
+  await browser.telemetry.registerEvents("test_category", {"test_category": {"methods":["click", "command"], "objects":["click_object"], "extra_keys":["selector"]}})
 
   // recording events
   browser.chromeWindows.onEvent.addListener(async (e) => {
     console.log(`event received: ${e}`);
     await browser.telemetry.recordEvent("test_category", "click", "click_object", JSON.stringify(e), {"selector": "dynamicSelector"});
+  }, "click");
+  browser.chromeWindows.onEvent.addListener(async (e) => {
+    console.log(`event received: ${e}`);
+    await browser.telemetry.recordEvent("test_category", "command", "click_object", JSON.stringify(e), {"selector": "dynamicSelector"});
   }, "command");
   // do "click" as well.
 })();
