8c252855f420137d7d5e80e985a16e8c4ac4af2e
Arthur Edelstein <arthuredelstein@gmail.com>
2020-03-23 00:05:47 -0700

    simplify

--- src/background.js
+++ src/background.js
@@ -2,16 +2,16 @@ console.log("heatmap started");
 
 (async () => {
   // registering telemetry events
-  await browser.telemetry.registerEvents("test_category", {"test_category": {"methods":["click", "command"], "objects":["click_object"], "extra_keys":["selector"]}})
+  await browser.telemetry.registerEvents("test_category", {"test_category": {"methods":["click", "command"], "objects":["click_object", "command_object"], "extra_keys":["selector"]}})
 
   // recording events
   browser.chromeWindows.onEvent.addListener(async (e) => {
     console.log(`event received: ${e}`);
-    await browser.telemetry.recordEvent("test_category", "click", "click_object", JSON.stringify(e), {"selector": "dynamicSelector"});
+    await browser.telemetry.recordEvent("test_category", "click", "click_object", JSON.stringify(e));
   }, "click");
   browser.chromeWindows.onEvent.addListener(async (e) => {
     console.log(`event received: ${e}`);
-    await browser.telemetry.recordEvent("test_category", "command", "click_object", JSON.stringify(e), {"selector": "dynamicSelector"});
+    await browser.telemetry.recordEvent("test_category", "command", "command_object", JSON.stringify(e));
   }, "command");
   // do "click" as well.
 })();
