65259ffef52a33396c3d69d798425e0b22e4bd9b
Arthur Edelstein <arthuredelstein@gmail.com>
2020-03-22 23:56:16 -0700

    get event cascade working

--- src/background.js
+++ src/background.js
@@ -1,20 +1,13 @@
-/*
-const winEnum = Services.wm.getEnumerator(null);
-const aWindow = winEnum.getNext();
-// Usually figures out the element involved in a click.
-aWindow.addEventListener("click", e => console.log("click from chrome window", e));
-aWindow.addEventListener("command", e => console.log("command received", e));
+console.log("heatmap started");
 
-// registering telemetry events
-await browser.telemetry.registerEvents("test_category", {"test_category": {"methods":["click"], "objects":["click_object"], "extra_keys":["selector"]}})
+(async () => {
+  // registering telemetry events
+  await browser.telemetry.registerEvents("test_category", {"test_category": {"methods":["click"], "objects":["click_object"], "extra_keys":["selector"]}})
 
-// recording events
-await browser.telemetry.recordEvent("test_category", "click", "click_object", "dynamicValue", {"selector": "dynamicSelector"})
-*/
-
-console.log("hello");
-
-//(async () => {
-//  console.log("devtools.chrome.enabled",
-//              await browser.chromeWindows.getBoolPref("devtools.chrome.enabled"));
-//})()
+  // recording events
+  browser.chromeWindows.onEvent.addListener(async (e) => {
+    console.log(`event received: ${e}`);
+    await browser.telemetry.recordEvent("test_category", "click", "click_object", JSON.stringify(e), {"selector": "dynamicSelector"});
+  }, "command");
+  // do "click" as well.
+})();
--- src/experiment_apis/chromeWindows/api.js
+++ src/experiment_apis/chromeWindows/api.js
@@ -1,12 +1,32 @@
 this.chromeWindows = class extends ExtensionAPI {
-  getAPI() {
+  getAPI(context) {
     const {Services} = ChromeUtils.import('resource://gre/modules/Services.jsm', {});
+    const { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm", {});
+    const { EventManager } = ExtensionCommon;
+    const initialWindows = [...Services.wm.getEnumerator(null)];
     return {
       chromeWindows: {
         async getBoolPref(prefName, defaultValue) {
           return Services.prefs.getBoolPref(prefName, defaultValue);
         },
-      },
-    };
+        onEvent: new EventManager({
+          context,
+          name: "chromeWindows.onEvent",
+          register: (fire, eventType) => {
+            const callback = event => {
+              Services.console.logStringMessage(`${eventType}: ${event}`);
+              fire.async({"type": event.type,
+                          "targetID": event.target.id});
+            };
+            initialWindows.map(w => w.addEventListener(eventType, callback));
+            Services.console.logStringMessage("event listeners added");
+            return () => {
+              initialWindows.map(w => w.removeEventListener(eventType, callback));
+              Services.console.logStringMessage("event listeners removed");
+            };
+          }
+        }).api()
+      }
+    }
   }
 };
