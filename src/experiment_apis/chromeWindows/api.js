7c6c2b10c1c6d498d587b750149c4aaa76fe507a
Arthur Edelstein <arthuredelstein@gmail.com>
2020-03-23 22:16:33 -0700

    handle new windows

--- src/experiment_apis/chromeWindows/api.js
+++ src/experiment_apis/chromeWindows/api.js
@@ -3,6 +3,7 @@ this.chromeWindows = class extends ExtensionAPI {
     const { Services } = ChromeUtils.import('resource://gre/modules/Services.jsm', {});
     const { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm", {});
     const { EventManager } = ExtensionCommon;
+    const {Cc: classes, Ci: interfaces} = Components;
     const initialWindows = [...Services.wm.getEnumerator(null)];
     return {
       chromeWindows: {
@@ -13,15 +14,30 @@ this.chromeWindows = class extends ExtensionAPI {
           context,
           name: "chromeWindows.onEvent",
           register: (fire, eventType) => {
-            const callback = event => {
+            const callback = (windowID, event) => {
               Services.console.logStringMessage(`${eventType}: ${event}`);
               fire.async({"type": event.type,
-                          "targetID": event.target.id,
-                          "tag": event.target.tagName});
+                          "id": event.target.id,
+                          "tag": event.target.tagName,
+                          "class": event.target.className,
+                          "windowID": windowID
+                         });
             };
-            initialWindows.map(w => w.addEventListener(eventType, callback));
+            const setupWindow = w => w.addEventListener(
+              eventType,
+              event => callback(w.windowUtils.outerWindowID, event));
+            const windowObserver = {
+              observe: function (subject, topic, data) {
+                if (topic === "domwindowopened") {
+                  setupWindow(subject);
+                }
+              }
+            };
+            initialWindows.map(setupWindow);
+            Services.ww.registerNotification(windowObserver);
             Services.console.logStringMessage("event listeners added");
             return () => {
+              Services.ww.unregisterNotification(windowObserver);
               initialWindows.map(w => w.removeEventListener(eventType, callback));
               Services.console.logStringMessage("event listeners removed");
             };
